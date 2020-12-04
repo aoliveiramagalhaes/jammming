4e2c7342805f722b9c089364fd075c9702b07e7b
Aline Magalhaes <aoliveiramagalhaes@gmail.com>
2020-12-03 22:09:27 -0500

    fixed task 37

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,7 +10,20 @@ class App extends React.Component {
     this.state = {
       SearchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
       { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
-      { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }]
+      { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }],
+      playlistName: 'My Playlist',
+      playlistTracks: [{
+        name: 'playlistName1', artist: 'playlistArtist1',
+        album: 'playlistAlbum1', id: 4
+      },
+      {
+        name: 'playlistName2', artist: 'playlistArtist2',
+        album: 'playlistAlbum2', id: 5
+      },
+      {
+        name: 'playlistName3', artist: 'playlistArtist3',
+        album: 'playlistAlbum3', id: 6
+      }]
     }
   }
 
@@ -22,7 +35,8 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults SearchResults={this.state.SearchResults} />
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
@@ -32,3 +46,22 @@ class App extends React.Component {
 
 
 export default App;
+
+
+// function App() {
+//   const playlistTracks = []
+//   Playlist(playlistTracks)
+// }
+
+// function Playlist(playlistTracks) {
+//   // param? 
+//   TrackList(playlistTracks)
+// }
+
+
+// function TrackList(tracks) {
+//   // param? 
+//   alert(tracks)
+// }
+
+// App()
