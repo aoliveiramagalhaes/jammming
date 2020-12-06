1e92476bf5bd7af054008e6259ceba1107023fb6
Aline Magalhaes <aoliveiramagalhaes@gmail.com>
2020-12-06 17:46:03 -0500

    comleted task 61

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -27,6 +27,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -46,6 +47,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
   render() {
     return (
       <div>
@@ -57,7 +62,8 @@ class App extends React.Component {
               onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
-              onRemove={this.removeTrack} />
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
