f9bbc26cfc2532ac9c1aa6c0d543c8fa49c9d2b4
Aline Magalhaes <aoliveiramagalhaes@gmail.com>
2020-12-06 19:02:25 -0500

    completed task 80

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -7,8 +7,18 @@ const Spotify = {
     }
 
     //check for access token match. This info is displayed in the hint (task 79)
-    const accessToken = window.location.href.match(/access_token=([^&]*)/);
+    const accessTokenMacth = window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMacth && expiresInMatch) {
+      accessToken = accessTokenMacth[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      // this clears the parameters, allowing us to grab a new access token when it expires. 
+      //Some details are displayed on task 80 hint
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    }
 
   }
 }
