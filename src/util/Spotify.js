11fe80b8e611eca960aaa37bab1413baa3b69570
Aline Magalhaes <aoliveiramagalhaes@gmail.com>
2020-12-06 19:54:23 -0500

    completed task 92

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -46,7 +46,25 @@ const Spotify = {
         uri: track.uri
       }));
     });
+  },
+
+  savePlayList(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+
+    return fetch('https://api.spotify.com/v1/me', { headers: headers }
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userId = jsonResponse.id;
+    })
+
   }
+
 };
 
 
