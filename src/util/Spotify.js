35f4ed9c7683ec6c339328e2865e90b3be3dd996
Aline Magalhaes <aoliveiramagalhaes@gmail.com>
2020-12-06 19:25:30 -0500

    completed task 86

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -24,7 +24,15 @@ const Spotify = {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
       window.location = accessUrl;
     }
+  },
 
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
   }
 }
 
